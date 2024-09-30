c857020d1ac6558777a9fac8277d603d576a3ffe
BlueSkeye <bsgithub@gmail.com>
2024-09-30 17:39:16 +0200

    UPGRADE : DotNetGraph from 2.7.0 to 3.2.0

--- Dna.ControlFlow/GraphVisualizer.cs
+++ Dna.ControlFlow/GraphVisualizer.cs
@@ -1,85 +1,73 @@
-﻿using DotNetGraph;
-using DotNetGraph.Edge;
-using DotNetGraph.Node;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DotNetGraph.Attributes;
+using DotNetGraph.Core;
 
 namespace Dna.ControlFlow
 {
     public static class GraphVisualizer
     {
-        /// <summary>
-        /// Gets a graphviz representation of the control flow graph.
-        /// </summary>
+        /// <summary>Gets a graphviz representation of the control flow graph.</summary>
         public static DotGraph GetDotGraph<T>(ControlFlowGraph<T> graph)
         {
             // Create a dotviz block for each basic block.
-            var dotGraph = new DotGraph(graph.Name, true);
-            var blockMapping = new Dictionary<BasicBlock<T>, DotNode>();
-            foreach(var block in graph.GetBlocks())
-            {
-                var node = GetDotNode(block);
+            // TODO : Unclear wether the true value is for Directed or Strict property.
+            DotGraph dotGraph = new DotGraph() {
+                Identifier = new DotIdentifier(graph.Name),
+                Directed = true
+            };
+            Dictionary<BasicBlock<T>, DotNode> blockMapping = new Dictionary<BasicBlock<T>, DotNode>();
+            foreach(BasicBlock<T> block in graph.GetBlocks()) {
+                DotNode node = GetDotNode(block);
                 blockMapping.Add(block, node);
                 dotGraph.Elements.Add(node);
             }
-
             // Create dotviz edges between basic blocks.
-            foreach(var block in graph.GetBlocks())
-            {
-                var outgoingEdges = block.GetOutgoingEdges().ToList();
-                for(int i = 0; i < outgoingEdges.Count; i++)
-                {
-                    var blockEdge = outgoingEdges[i];
-                    var lineColor = GetEdgeColor(i, outgoingEdges.Count);
-                    var dotEdge = GetDotEdge(blockMapping[blockEdge.TargetBlock], blockMapping[blockEdge.SourceBlock], lineColor);
+            foreach(BasicBlock<T> block in graph.GetBlocks()) {
+                List<BlockEdge<T>> outgoingEdges = block.GetOutgoingEdges().ToList();
+                for(int i = 0; i < outgoingEdges.Count; i++) {
+                    BlockEdge<T> blockEdge = outgoingEdges[i];
+                    DotColor lineColor = GetEdgeColor(i, outgoingEdges.Count);
+                    DotEdge dotEdge = GetDotEdge(blockMapping[blockEdge.TargetBlock],
+                        blockMapping[blockEdge.SourceBlock], lineColor);
                     dotGraph.Elements.Add(dotEdge);
                 }
             }
-
             return dotGraph;
         }
 
         private static DotNode GetDotNode<T>(BasicBlock<T> block)
         {
-            return new DotNode(block.Name)
-            {
-                Shape = DotNodeShape.Box,
+            return new DotNode() {
+                Color = new DotColor(255, 66, 66, 66),
+                FontColor = DotColor.Black,
+                Identifier = new DotIdentifier(block.Name),
                 Label = GraphFormatter.FormatBlock(block),
-                Color = Color.FromArgb(255, 66, 66, 66),
-                FontColor = Color.Black,
+                Shape = DotNodeShape.Box,
             };
         }
 
-        /// <summary>
-        /// Gets a line color for the provided edge.
-        /// </summary>
-        private static Color GetEdgeColor(int index, int count)
+        /// <summary>Gets a line color for the provided edge.</summary>
+        private static DotColor GetEdgeColor(int index, int count)
         {
-            var thenColor = Color.Green;
-            var elseColor = Color.Red;
-            var switchColor = Color.LightBlue;
+            DotColor thenColor = DotColor.Green;
+            DotColor elseColor = DotColor.Red;
+            DotColor switchColor = DotColor.LightBlue;
 
-            if (count > 2)
+            if (count > 2) {
                 return switchColor;
-            if (index == 0)
-                return thenColor;
-            else
-                return elseColor;
+            }
+            return (index == 0) ? thenColor : elseColor;
         }
 
-        private static DotEdge GetDotEdge(DotNode from, DotNode to, Color lineColor)
+        private static DotEdge GetDotEdge(DotNode from, DotNode to, DotColorAttribute lineColor)
         {
-            return new DotEdge(to, from)
-            {
+            return new DotEdge() {
                 ArrowHead = DotEdgeArrowType.Normal,
                 ArrowTail = DotEdgeArrowType.None,
                 Color = lineColor,
-                Label = String.Empty,
+                From = from.Identifier,
+                Label = string.Empty,
                 Style = DotEdgeStyle.Solid,
+                To = to.Identifier
             };
         }
     }
